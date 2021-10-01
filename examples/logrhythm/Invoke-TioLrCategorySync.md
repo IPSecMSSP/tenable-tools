c34bc48e31f79529f41666c7f1b36a8dfa3e51aa
John Berkers <jberkers@ipsec.com.au>
2021-10-01 17:02:12 +1000

    Add script to sync Tenable.IO Categories to LR List

---
+++ examples/logrhythm/Invoke-TioLrCategorySync.md
@@ -0,0 +1,132 @@
+---
+external help file: Tenable.Tools-help.xml
+Module Name: Tenable.Tools
+schema: 2.0.0
+---
+
+# Invoke-TioLrCategorySync
+
+## SYNOPSIS
+
+Synchronise Tenable.io Tag Categories with LogRhythm Lists.
+
+## SYNTAX
+
+```powershell
+Invoke-TioLrCategorySync.ps1 [-ApiKeys] <psobject> [[-ListPrefix] <string>] [[-EntityName] <string>] [-ListOwner] <Object> [[-ListReadAccess] <string>] [[-ListWriteAccess] <string>] [-HostnameOnly] [-IPv4Only] [<CommonParameters>]
+```
+
+## DESCRIPTION
+
+This script allows for the assets with specific Tag Categories to be synchronised to a LogRhythm list, using either the Tenable Asset's `hostname` or `ipv4`, or both, properties.
+
+This script assumes you have LogRhythm.Tools installed and configured in accordance with it's documentation.  It also defaults to using the ***cloud.tenable.com*** hostname to access the Tenable.io APIs.
+
+## EXAMPLES
+
+### Example 1
+
+```powershell
+PS > $TioApiKeys = @{}
+PS > $TioApiKeys.Add('AccessKey',(Get-Credential -UserName 'Access Key'))
+PS > $TioApiKeys.Add('SecretKey',(Get-Credential -UserName 'Secret Key'))
+PS > Invoke-TioLrCategorySync -ApiKeys $TioApiKeys -ListPrefix 'Example TIO' -Entity Name 'Example' -ListOwner 1 -ListReadAccess 'publicrestrictedanalyst' -ListWriteAccess 'publicglobaladministrator' -HostnameOnly
+```
+
+Perform Tenable IO Tag Category Synchronisation, setting the List Prefix to ***Entity TIO***, lists to be created in entity ***Example***, with list owner ID 1.  Read permissions set to ***PublicRestrictedAnalyst**, write permission for ***PublicGlobalAdministrators***.
+
+## PARAMETERS
+
+### ApiKeys
+
+PSObject containing two PSCredential Objects named **AccessKey** and **SecretKey** with the respective Access Key and Secret Key stored in the password property of these.
+
+### ListPrefix
+
+Prefix to use with lists created by the script.  Defaults to ***TIO***.
+
+### EntityName
+
+Entity to assign list to.  This has an effect on who can Read/Write the list, depending on permissions assigned.  Specifically, for ***restricted*** Access values, users in the same entity whith the same or higher permission can interact with the list in the way specified.
+
+Defalts to ***Primary Site***
+
+***Ie:*** `-ListReadAccess restrictedanalyst` means that both RestictedAnalyst and RestrictedAdmin in the same Entity can see the list.
+
+### ListOwner
+
+Id or Name of owner to assign to list.
+
+***Note:*** Due to a limitation in the `lr-admin-api`, if the PersonID and UserID values are out-of-sync in the LogRhythmEMDB database, specifying the name does not work as the list requires the `PersonID` and the `lr-admin-api` only returns the `UserID`.  To work around this, specify the numeric value of the intended user's `PersonID`.
+
+### ListReadAccess
+
+Read permissions to apply to list.  One of:
+
+* private
+* publicall
+* publicglobaladmin
+* publicgolbalanalyst
+* publicrestrictedadmin
+* publicrestrictedanalyst
+
+Defaults to **publicglobalanalyst**.
+
+### ListWriteAccess
+
+Write permissions to apply to list.  One of:
+
+* private
+* publicall
+* publicglobaladmin
+* publicgolbalanalyst
+* publicrestrictedadmin
+* publicrestrictedanalyst
+
+Defaults to **publicglobaladmin**
+
+### HostnameOnly
+
+Add only the Tenable.io hostname asset property to the lists(s).
+
+### IPv4Only
+
+Add only the Tenable.io ipv4 asset property to the list(s).
+
+### CommonParameters
+
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### None
+
+## OUTPUTS
+
+Contents in LogRhythm Lists.
+
+## NOTES
+
+This synchronisation script makes use of LogRhythm Lists to store the configuration related to which Tag Categories to synchronise.  On first run it will create the two Configuration Lists:
+
+* TIO : Conf : Tag Category Available
+* TIO : Conf : Tag Category Enabled
+
+Values from the **Tag Category Available** list can be copied/pasted to the **Tag Category Enabled** list for the next run of the script.
+
+For each Tag Category that is in the **Tag Category Enabled** list the script will get a list of available **Tag Values**.  Each Category/Value combination will result in the creation of a List in LogRhythm, conforming to the naming convention:
+
+* TIO : Tag : `Category Name` : `Tag Value`
+
+Eg.
+
+* TIO : Tag : Windows Production Server : Physical
+* TIO : Tag : Windows Production Server : Site A Virtual
+* TIO : Tag : Windows Production Server : Site B Virtual
+
+Each of these will be populated with entries obtained using the Tenable.IO Export Asset API, using a Tag Category filter.  Depending on whether either **HostnameOnly** or **IPv4Only** switch parameters are specified, either the hostname, ipv4 or both are added to the list.  To support this, the list is of type **Host**, allowing both data types to be present.
+
+## RELATED LINKS
+
+[Tenable Developer API Reference](https://developer.tenable.com/reference)
+[LogRhythm Tools PowerShell Module](https://github.com/LogRhythm-Tools/LogRhythm.Tools)
---
+++ examples/logrhythm/Invoke-TioLrCategorySync.ps1
@@ -0,0 +1,217 @@
+<#
+  .SYNOPSIS
+    Synchronise Tenable.io Tag Cagegories into LogRhythm Lists
+  .DESCRIPTION
+    Tenable.IO allows organisations to create Tag Categories that may be used to set up scheduled scans, etc.  Each Tag Category has one or more values associated with it.
+
+    Tenable.IO also allows these to be dynamically updated based on properties of assets that may come about as a result of a discovery scan, or other scans.
+
+    This script begins by pulling a list of Tag Categories from Tenable.IO and storing these in a "Configuration" list in LogRhythm.
+    A Subsequent list allows the tag categories that are to be synchronised to be specified by adding these to the list.
+
+    The Synchronisation, when run, perform the following tasks:
+
+    * Synchronise the list of available Tag Categories and save them in a list.
+    * Iterate through the values in the "enabled" list, and synchronise those Tag Categories
+      * Each Value within the Tag Category will result in a separate list
+
+    List Naming Convention is as follows:
+
+    TIO : Conf : Tag Category Available
+    TIO : Conf : Tag Category Enabled
+    TIO : Tag : Tag Category : Tag Value
+
+    The List Prefix can be specified as a parameter and defaults to 'TIO'
+  .PARAMETER ApiKeys
+    PSObject containing PSCredential Objects with AccessKey and SecretKey.
+    Must contain PSCredential Objects named AccessKey and SecretKey with the respective keys stored in the Password property
+  .PARAMETER ListPrefix
+    The Prefix to use on all lists created and managed by this invokation.
+
+    Default: 'TIO'
+  .PARAMETER EntityName
+    Name of Entity in LogRhythm that will be set as the 'Entity' for the list.  Determines list visibility when used with Restricted Access types.
+  .PARAMETER ListOwner
+    Display Name of the 'Person' in LogRhythm that will be set as the list owner.
+  .PARAMETER ListReadAccess
+    Level of Read Permission to assign to new list.  Controls visibility of list to other users.
+
+    Default: PublicGlobalAnalyst
+  .PARAMETER ListWriteAccess
+    Level or Write Permisssions to assign to new list.  Controls who is able to make changes to the list.
+
+    Default: PublicGlobalAdmin
+  .PARAMETER HostnameOnly
+    Add only hostnames to the list
+  .PARAMETER IPv4Only
+    Add only IPv4 Addresses to the list
+  .NOTES
+    This script requires the presence of both the LogRhythm.Tools and Tenable.Tools PowerShell Modules. LogRhythm.Tools module must already be configured for the user running this script.
+#>
+
+param(
+  [Parameter(Mandatory=$true)]
+  [psobject]$ApiKeys,
+
+  [Parameter(Mandatory=$false)]
+  [string]$ListPrefix = 'TIO',
+
+  [Parameter(Mandatory=$false)]
+  [string]$EntityName = 'Primary Site',
+
+  [Parameter(Mandatory=$true)]
+  $ListOwner,
+
+  [Parameter(Mandatory=$false)]
+  [ValidateSet('private','publicall', 'publicglobaladmin', 'publicglobalanalyst', 'publicrestrictedadmin', `
+  'publicrestrictedanalyst', ignorecase=$true)]
+  [string]$ListReadAccess = 'PublicGlobalAnalyst',
+
+  [Parameter(Mandatory=$false)]
+  [ValidateSet('private','publicall', 'publicglobaladmin', 'publicglobalanalyst', 'publicrestrictedadmin', `
+  'publicrestrictedanalyst', ignorecase=$true)]
+  [string]$ListWriteAccess = 'PublicGlobalAdmin',
+
+  [Parameter(Mandatory=$false)]
+  [switch]$HostnameOnly,
+
+  [Parameter(Mandatory=$false)]
+  [switch]$IPv4Only
+)
+
+#region List Owner Details
+if ($ListOwner -is [int]) {
+  # If the ListOwner is an integer, use that as the ID
+  $ListOwnerId = $ListOwner
+  Write-Verbose "$(Get-Timestamp)List Owner ID: $ListOwnerId"
+} else {
+  $ListOwnerResults = Get-LrUsers -Name $ListOwner -Exact
+
+  if ($ListOwnerResults) {
+    $ListOwnerId = $ListOwnerResults | Select-Object -ExpandProperty number
+    Write-Verbose "$(Get-Timestamp)List Owner ID: $ListOwnerId"
+  } else {
+    return "$(Get-timestamp) Error - Unable to retrieve user identity ID for user: $ListOwner"
+  }
+}
+
+#endregion
+
+#region Available/Enabled Tag Categories
+
+# Configuration List Names
+$ListTioTagCategoryAvailable = '{0} : Conf : Tag Category Available' -f $ListPrefix
+$ListTioTagCategoryEnabled = '{0} : Conf : Tag Category Enabled' -f $ListPrefix
+
+# Available Tag Categories
+# Determine if LR List exists
+$ListStatus = Get-LrLists -Name $ListTioTagCategoryAvailable -Exact
+
+# Create the list if it does not exist
+if (!$ListStatus) {
+    Write-Verbose "$(Get-TimeStamp) - Create New List: $ListTioTagCategoryAvailable."
+    New-LrList -Name $ListTioTagCategoryAvailable -ListType "generalvalue" -UseContext "message" -ShortDescription "List of available Tenable.IO Tag Categories.  Do not modify this list manually." -ReadAccess $ListReadAccess -WriteAccess $ListWriteAccess -Owner $ListOwnerId -EntityName $EntityName
+} else {
+    Write-Verbose "$(Get-TimeStamp) - List Verification: $ListTioTagCategoryAvailable exists.  Synchronizing contents between Tenable.IO and this LogRhythm list."
+}
+
+# Sync Available Tag Categories
+Try {
+  $TioTagCategories = Get-TioTagCategory -ApiKeys $ApiKeys
+} Catch {
+  Write-Error "$(Get-timestamp) - Unable to retrieve Tenable.IO Tag Categories. See Get-TioTagCategory"
+}
+
+$TioTagCategoryNames = ($TioTagCategories | Select-Object -Property 'name').name
+
+Write-Verbose "$(Get-TimeStamp) - Updating List Contents: $ListTioTagCategoryAvailable."
+Sync-LrListItems -Name $ListTioTagCategoryAvailable -Value $TioTagCategoryNames
+
+#Enabled Tag Categories
+# Determine if LR List exists
+$ListStatus = Get-LrLists -Name $ListTioTagCategoryEnabled -Exact
+
+# Create the list if it does not exist
+if (!$ListStatus) {
+  Write-Verbose "$(Get-TimeStamp) - Create New List: $ListTioTagCategoryEnabled."
+  New-LrList -Name $ListTioTagCategoryEnabled -ListType "generalvalue" -UseContext "message" -ShortDescription "List of enabled Tenable.IO Tag Categories.  Modify this list manually from values in $ListTioTagCategoryAvailable" -ReadAccess $ListReadAccess -WriteAccess $ListWriteAccess -Owner $ListOwnerId -EntityName $EntityName
+} else {
+  Write-Verbose "$(Get-TimeStamp) - List Verification: $ListTioTagCategoryEnabled exists."
+}
+
+#endregion
+
+#region Value Sync - Tag Categories
+
+# List of Enabled Tag Categories
+$EnabledTioTagCategories = Get-LrlIstItems -Name $ListTioTagCategoryEnabled -Exact -ValuesOnly
+
+if($EnabledTioTagCategories) {
+  Write-Output "$(Get-TimeStamp) - Begin - Tenable.IO Tag Category Sync"
+
+  foreach ($TioTagCategory in $EnabledTioTagCategories) {
+    # Get available Values for this Tag Category
+    Write-Verbose "$(Get-TimeStamp) - Synchronising Tag Category: $TioTagCategory"
+    $TagCategoryValues = Get-TioTagValue -ApiKeys $ApiKeys -CategoryName $TioTagCategory
+    $TagCategoryValueIds = ($TagCategoryValues).uuid
+    $TagCategoryValueNames = ($TagCategoryValues).value
+
+    foreach ($TagUuid in $TagCategoryValueIds) {
+      # Construct List Name
+      $TioTagValue = Get-TioTagValue -ApiKeys $ApiKeys -Uuid $TagUuid
+      $ListTioCategoryValueName = (Get-Culture).TextInfo.ToTitleCase(('{0} : Tag : {1} : {2}' -f $ListPrefix, $TioTagCategory, $TioTagValue.value))
+
+      # Check for existence of list
+      $ListStatus = Get-LrLists -Name $ListTioCategoryValueName -Exact
+
+      # Create the list if it does not exist, else update it
+      $ShortDescription = 'Tenable.IO Tag Category: {0}; Tag Value: {1}.  Do not modify this list manually, it is Synchronised from Tenable.IO.' -f $TioTagValue.category_description, $TioTagValue.description
+      if (!$ListStatus) {
+        Write-Verbose "$(Get-TimeStamp) - Creating List: $ListTioCategoryValueName."
+        New-LrList -Name $ListTioCategoryValueName -ListType 'host' -ShortDescription $ShortDescription -ReadAccess $ListReadAccess -WriteAccess $ListWriteAccess -Owner $ListOwnerId -EntityName $EntityName
+      } else {
+        Write-Verbose "$(Get-TimeStamp) - List Verification: $ListTioCategoryValueName exists, updating."
+        Update-LrList -Guid $ListStatus.guid -Name $ListTioCategoryValueName -ListType 'host' -ShortDescription $ShortDescription -ReadAccess $ListReadAccess -WriteAccess $ListWriteAccess -Owner $ListOwnerId -EntityName $EntityName
+      }
+
+      # Get entries from Tenable.IO
+      Write-Verbose "$(Get-TimeStamp) - Running: Get-TioExportAsset -TagCategory $($TioTagValue.category_name) -TagValue $($TioTagValue.value)"
+      $AssetList = Get-TioExportAsset -ApiKeys $ApiKeys -TagCategory $TioTagValue.category_name -TagValue $TioTagValue.value
+
+      if (!$HostnameOnly) {
+        # Build (a) list(s) of values to load into the list.
+        $IPv4List = ($AssetList | Select-Object -Property 'ipv4s').ipv4s
+
+        # Populate list with IPs
+        if($IPv4List.Count -gt 0) {
+          Write-Information "$(Get-TimeStamp) - Syncing Quantity: $($IPv4List.Count) IPv4 to List $ListTioCategoryValueName"
+          Sync-LrListItems -Value $IPv4List -Name $ListTioCategoryValueName
+        } else {
+          Write-Information "$(Get-TimeStamp) - IPv4 Quantity: $($IPv4List.Count)"
+        }
+      }
+
+      If(!$IPv4Only) {
+        $HostnameList = ($AssetList | Select-Object -Property 'hostnames').hostnames
+
+        # Populate list with HostNames
+        if($HostnameList.Count -gt 0) {
+          Write-Information "$(Get-TimeStamp) - Syncing Quantity: $($HostnameList.Count) IPv4 to List $ListTioCategoryValueName"
+          Sync-LrListItems -Value $HostnameList -Name $ListTioCategoryValueName
+        } else {
+          Write-Information "$(Get-TimeStamp) - IPv4 Quantity: $($HostnameList.Count)"
+        }
+      }
+
+      Write-Verbose "$(Get-Timestamp) - Memory Management: Clearing Variables"
+      Clear-Variable AssetList -ErrorAction SilentlyContinue
+      Clear-Variable IPv4List -ErrorAction SilentlyContinue
+      Clear-Variable HostnameList -ErrorAction SilentlyContinue
+      Clear-Variable ListStatus -ErrorAction SilentlyContinue
+      [GC]::Collect()
+    }
+    Write-Output "$(Get-TimeStamp) - End - Tenable.IO Tag Category Sync"
+  }
+}
+
+#endregion
